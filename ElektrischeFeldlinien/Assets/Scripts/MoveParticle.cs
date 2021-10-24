ca74d5e76bdce39a73e12a91a0c8693ddaadd375
ArthurSchwarze <arthur.schwarze@students.ksba.ch>
2021-10-24 12:14:11 +0200

    NewChanges

--- ElektrischeFeldlinien/Assets/Scripts/MoveParticle.cs
+++ ElektrischeFeldlinien/Assets/Scripts/MoveParticle.cs
@@ -4,48 +4,103 @@ using UnityEngine;
 
 public class MoveParticle : MonoBehaviour
 {
-    public Vector3 particleMovement;
+    public Vector3 movementVector;
     public Vector3 sumOfCharge;
 
-    public Rigidbody rb;
+    public LineRenderer lineRenderer;
 
     private Data worldData;
    
     private double e0;
     private float constantChange;
+    private int lineCount = 1;
+    private bool drawAndMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
+        lineRenderer = transform.GetComponent<LineRenderer>();
         worldData = GameObject.Find("World").GetComponent<Data>();
+        lineRenderer.positionCount = lineCount;
+        lineRenderer.SetPosition(0, transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        sumOfCharge = Vector3.zero;
-        
-        e0 = worldData.e0;
-        constantChange = (float)(1 / (4 * e0 * Mathf.PI));
-
-        Vector3 Cor = transform.position;
-
-        for (int i = 0; i < worldData.objectCount; i++) 
+        if (drawAndMove) 
         {
-            ObjectAttributes chargedObject = worldData.chargedObjects[i];
-            float charge = chargedObject.charge;
-
-            Vector3 CorChar = chargedObject.transform.position;
-            Vector3 CorDiff = Cor - CorChar;
-
-            sumOfCharge += ((charge) / Mathf.Pow((Mathf.Pow(CorDiff.x, 2f) + Mathf.Pow(CorDiff.y, 2f) + Mathf.Pow(CorDiff.z, 2f)), 3f / 2f)) * new Vector3(CorDiff.x, CorDiff.y, CorDiff.z);
+            MoveAndDraw();
         }
-       
-        Vector3 movementVector = constantChange * sumOfCharge * Time.deltaTime / 1000000;
+    }
 
-        rb.velocity = movementVector;
+    private void MoveAndDraw() 
+    {
+        while (true)
+        {
+            sumOfCharge = Vector3.zero;
 
+            e0 = worldData.e0;
+            constantChange = (float)(1 / (4 * e0 * Mathf.PI));
 
-        //particleMovement = 
+            Vector3 Cor = transform.position;
+
+            for (int i = 0; i < worldData.objectCount; i++)
+            {
+                ObjectAttributes chargedObject = worldData.chargedObjects[i];
+                float charge = chargedObject.charge;
+
+                Vector3 CorChar = chargedObject.transform.position;
+                Vector3 CorDiff = Cor - CorChar;
+
+                sumOfCharge += ((charge) / Mathf.Pow((Mathf.Pow(CorDiff.x, 2f) + Mathf.Pow(CorDiff.y, 2f) + Mathf.Pow(CorDiff.z, 2f)), 3f / 2f)) * new Vector3(CorDiff.x, CorDiff.y, CorDiff.z);
+            }
+
+            movementVector = sumOfCharge * Time.deltaTime * 100f;
+
+            var Coordinates = new List<float> { Mathf.Abs(movementVector.x), Mathf.Abs(movementVector.y), Mathf.Abs(movementVector.z) };
+
+            float maxValue = Mathf.Max(Coordinates.ToArray());
+            if (maxValue == 0)
+            {
+                maxValue = 1 / 1000;
+            }
+
+            int position = Coordinates.IndexOf(maxValue);
+
+            //rb.velocity = movementVector;
+
+            Vector3 balancesMovement = movementVector / Mathf.Abs(maxValue * 5);
+
+            transform.position += balancesMovement;
+
+            lineCount++;
+
+            if (lineCount < 450) //2450 originally
+            {
+                lineRenderer.positionCount = lineCount;
+                lineRenderer.SetPosition(lineCount - 1, balancesMovement + Cor);
+            }
+            else
+            {
+                drawAndMove = false;
+                Debug.Log(lineCount);
+                GetComponent<MeshRenderer>().enabled = false;
+                return;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var chargeScript = collision.gameObject.GetComponent<ObjectAttributes>();
+        
+        if (chargeScript)
+        {
+            if (chargeScript.charge == -1)
+            {
+                lineCount = 2500;
+            }
+        }
     }
 }
